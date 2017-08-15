6af5dd80e8bd76223d73eeadb46cf4b89448322c
HeavenVolkoff <vasvas10@gmail.com>
2017-08-15 10:21:04 -0300

    First working draft

---
+++ Selector.js
@@ -0,0 +1,125 @@
+'use strict'
+
+var path = require('path')
+var cheerio = require('cheerio')
+
+// Constants
+var isArray = Array.isArray
+var tagRegex = /^<(\S+)>$/
+var TAG_DEFAULT = {
+  a: ['href'],
+  img: ['src'],
+  link: ['href'],
+  embed: ['src'],
+  track: ['src'],
+  video: ['src', '<source>', '<track>'],
+  audio: ['src', '<source>', '<track>'],
+  script: ['src'],
+  source: ['src'],
+  object: ['data']
+}
+
+function Selector (_tagName, _propName, _ext) {
+  var ext = typeof _ext === 'string' ? _ext : ''
+  var tagName = typeof _tagName === 'string' ? _tagName.toLowerCase() : ''
+  var propName
+
+  if (typeof _propName === 'string') {
+    if (_propName.length > 0) {
+      if (_ext === undefined && _propName[0] === '.') {
+        ext = _propName
+        propName = null
+      } else {
+        propName = [_propName]
+      }
+    }
+  } else if (isArray(_propName)) {
+    propName =
+      _propName.length === 0
+        ? null
+        : _propName.map(function mapPropertiesArray (prop) {
+          return prop instanceof Selector ? prop : prop + ''
+        })
+  }
+
+  if (!tagName) throw new Error('Tag name is required')
+  if (ext && (ext.length < 2 || ext[0] !== '.')) {
+    throw new TypeError('Invalid extension type')
+  }
+
+  this.ext = ext || null
+  this.tagName = tagName
+  this.propName = propName || TAG_DEFAULT[tagName] || null
+}
+
+Selector.isSelectorArray = function isSelectorArray (arr) {
+  if (!isArray(arr)) return false
+
+  var length = arr.length
+  while (--length >= 0 && arr[length] instanceof Selector) {}
+
+  return length === 0
+}
+
+Selector.prototype = {
+  retrieveIn: function retriveIn ($, fatherSelector, cb) {
+    var ext = this.ext || (fatherSelector && fatherSelector.ext) || null
+    var elements = $(this.tagName)
+    var filePaths = []
+    var elementsLength = elements.length
+    for (var i = 0; i < elementsLength; ++i) {
+      var element = $(elements[i])
+      var propNames = this.propName
+      var propNamesLength = propNames.length
+      for (var j = 0; j < propNamesLength; ++j) {
+        var propName = propNames[j]
+        if (typeof propName === 'string') {
+          var tempProp = tagRegex.exec(propName)
+
+          if (tempProp === null) {
+            tempProp = element.prop(propName) + ''
+
+            if (tempProp && (!ext || path.extname(tempProp) === ext)) {
+              filePaths.push(
+                typeof cb === 'function'
+                  ? cb(element, propName, tempProp)
+                  : tempProp
+              )
+              break
+            }
+          } else {
+            tempProp = new Selector(tempProp[1]).retrieveIn(
+              function (tagName) {
+                return cheerio(tagName, elements)
+              },
+              this,
+              cb
+            )
+
+            if (tempProp.length > 0) {
+              filePaths.concat(tempProp)
+              break
+            }
+          }
+        } else {
+          tempProp = propName.retrieveIn(
+            function (tagName) {
+              return cheerio(tagName, elements)
+            },
+            this,
+            cb
+          )
+
+          if (tempProp.length > 0) {
+            filePaths.concat(tempProp)
+            break
+          }
+        }
+      }
+    }
+
+    return filePaths
+  }
+}
+
+module.exports = Selector
---
+++ index.js
@@ -0,0 +1,182 @@
+'use strict'
+
+var fs = require('fs')
+var gUtil = require('gulp-util').PluginError
+var cheerio = require('cheerio')
+var through = require('through2')
+var Selector = require('./Selector')
+var PLUGIN_NAME = require('./package.json').name
+
+if (!Buffer.from) {
+  Buffer.from = function (arg) {
+    return new Buffer(arg)
+  }
+}
+if (!Array.from) {
+  Array.from = function (arg) {
+    for (var i = 0, arr = [], length = arg.length; i < length; ++i) {
+      arr[i] = arg[i]
+    }
+    return arr
+  }
+}
+
+var log = gUtil.log
+var PluginError = gUtil.PluginError
+
+var readFileOpts = {
+  flag: 'r',
+  encoding: null
+}
+
+function fileExists (path, mode) {
+  return new Promise(function fileExistsExecutor (resolve) {
+    fs.access(path, mode, function (err) {
+      resolve(!err)
+    })
+  })
+}
+
+function readFile (path) {
+  return new Promise(function readFileContent (resolve, reject) {
+    fs.readFile(path, readFileOpts, function (err, data) {
+      if (err) return reject(err)
+      resolve(data)
+    })
+  })
+}
+
+function genVinylFileObj (cwd, base, path) {
+  return fileExists(path, fs.constants.R_OK)
+    .then(function (exists) {
+      if (!exists) {
+        log('File: ' + path + " isn't accessible")
+        return null
+      }
+
+      return readFile(path).catch(function () {
+        log("Couldn't read file: " + path)
+        return null
+      })
+    })
+    .then(function toVinyl (data) {
+      if (!data) return null
+
+      return new gUtil.File({
+        cwd: cwd,
+        base: base,
+        path: path,
+        contents: data
+      })
+    })
+}
+
+function parseSelectorArguments (selectors) {
+  if (selectors instanceof Selector) {
+    if (arguments.length > 1) {
+      return parseSelectorArguments(Array.from(arguments))
+    }
+    return [selectors]
+  } else if (Selector.isSelectorArray(selectors)) {
+    return selectors
+  } else if (typeof selectors === 'string') {
+    return parseSelectorArguments(
+      new Selector(arguments[0], arguments[1], arguments[2])
+    )
+  }
+
+  throw new PluginError(
+    PLUGIN_NAME,
+    'Argument must be a selector or selector array'
+  )
+}
+
+module.exports = function retriever () {
+  var selectors = parseSelectorArguments.apply(null, arguments)
+  var length = selectors.length
+
+  return through.obj(function (file, enc, cb) {
+    var i, root, gulp, files
+    gulp = this
+
+    if (file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, 'Streams are not supported!'))
+    }
+
+    if (file.isBuffer()) {
+      root = cheerio.load(file.contents)
+      files = []
+      for (i = 0; i < length; ++i) files.concat(selectors[i].retrieveIn(root))
+
+      return Promise.all(files.map(genVinylFileObj.bind(file.cwd, file.base)))
+        .then(function (files) {
+          var i, length
+          for (i = 0, length = files.length; i < length; ++i) {
+            if (files[i]) gulp.push(files[i])
+          }
+          cb()
+        })
+        .catch(function (error) {
+          cb(new PluginError(PLUGIN_NAME, error + ''))
+        })
+    }
+
+    this.push(file)
+    cb()
+  })
+}
+
+module.exports.htmlRename = function htmlRename () {
+  var selectors = Array.from(arguments)
+  var renameFunc = selectors.pop()
+
+  if (typeof renameFunc !== 'function') {
+    throw new PluginError(
+      PLUGIN_NAME,
+      'Last argument must be a rename function'
+    )
+  }
+
+  selectors = parseSelectorArguments.apply(null, selectors)
+  var length = selectors.length
+
+  return through.obj(function (file, enc, cb) {
+    var i, root, files
+
+    if (file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, 'Streams are not supported!'))
+    }
+
+    if (file.isBuffer()) {
+      root = cheerio.load(file.contents)
+      files = []
+      for (i = 0; i < length; ++i) {
+        files.concat(
+          selectors[i].retrieveIn(root, null, function rename (
+            element,
+            prop,
+            filePath
+          ) {
+            return fileExists(filePath, fs.constants.R_OK).then(function (
+              exists
+            ) {
+              if (exists) element.prop(prop, renameFunc(filePath))
+            })
+          })
+        )
+      }
+
+      return Promise.all(files)
+        .then(function () {
+          file.contents = Buffer.from(root.html())
+          cb(null, file)
+        })
+        .catch(function (error) {
+          cb(new PluginError(PLUGIN_NAME, error + ''))
+        })
+    }
+
+    this.push(file)
+    cb()
+  })
+}
